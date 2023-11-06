26d12189785e347a85761d2eace3cc703907f419
Ramon253 <jgalazc253@g.educaand.es>
2023-11-06 14:52:37 +0100

    File generator terminado, añadido php y css

--- htmlSample.js
+++ htmlSample.js
@@ -1,20 +1,25 @@
-exports.getHtml = (fileName, fileNumber, script = false) => {
-    const html = `<!Doctype html>
+exports.getHtml = (fileName, fileNumber, script = false,css = false) => {
+    let html = `<!Doctype html>
 <html lang="en">
     <head>
     <meta charset="UTF-8">
     <meta name="viewport"
     content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>${fileName}${fileNumber}</title>
-</head>
-<body>`
-    let scriptTag = ""
-    if (script){
-        scriptTag += `<script src="${fileName}${fileNumber}.js"></script>`
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">`
+
+    if (css){
+        html += `<link rel="stylesheet" href="${fileName}${fileNumber}.css">`
     }
 
-    return html + scriptTag +`
+    html += `
+<title>${fileName}${fileNumber}</title>
+</head>
+<body>`
+
+    if (script){
+        html += `<script src="${fileName}${fileNumber}.js"></script>`
+    }
+    return html+ `
 </body>
 </html>`
 }
