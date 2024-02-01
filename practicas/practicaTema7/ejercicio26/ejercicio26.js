b4f3f435d4048218bbd8f424692cab475fc41935
Ramon253 <jgalazc253@g.educaand.es>
2024-02-01 14:24:42 +0100

    T7 ultimo ejercicio empezado

--- practicas/practicaTema7/ejercicio26/ejercicio26.js
+++ practicas/practicaTema7/ejercicio26/ejercicio26.js
@@ -1,4 +1,19 @@
-let deck = document.getElementById('deck');
-function startGame(deck){
+let deck = document.querySelectorAll('#deck > div')
 
+deck.forEach((element) => {
+    element.addEventListener('dragstart', ev => moveElement(element, ev))
+    element.addEventListener('dragover', ev => {
+        console.log('Adios')
+    })
+    element.addEventListener('drop', () => {
+
+    })
+})
+
+function startGame(deck) {
+
+}
+
+function moveElement(element, ev) {
+    console.log('Hola')
 }
