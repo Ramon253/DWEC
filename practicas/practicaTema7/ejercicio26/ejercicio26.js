7daed5650e9c49d279f22ef0c720b85ca4666209
Ramon253 <jgalazc253@g.educaand.es>
2024-02-08 14:49:18 +0100

    Card game casi terminao

--- practicas/practicaTema7/ejercicio26/ejercicio26.js
+++ practicas/practicaTema7/ejercicio26/ejercicio26.js
@@ -1,19 +1,94 @@
 let deck = document.querySelectorAll('#deck > div')
+let colorsShowed = 0
+let onDragElement = ''
+let colors = [
+    '#FF5733',
+    '#33FF68',
+    '#3361FF',
+    '#B733FF',
+    '#FFFF33',
+    '#33FFFF',
+    '#FF33B7',
+    '#FF3333',
+    '#33FFAA',
+    '#B73333',
+    '#33B7FF',
+    '#FF33FF',
+    '#33FF33',
+    '#3333FF',
+    '#FFB733',
+    '#FF5733',
+    '#33FF68',
+    '#3361FF',
+    '#B733FF',
+    '#FFFF33',
+    '#33FFFF',
+    '#FF33B7',
+    '#FF3333',
+    '#33FFAA',
+    '#B73333',
+    '#33B7FF',
+    '#FF33FF',
+    '#33FF33',
+    '#3333FF',
+    '#FFB733'
+]
 
-deck.forEach((element) => {
-    element.addEventListener('dragstart', ev => moveElement(element, ev))
-    element.addEventListener('dragover', ev => {
-        console.log('Adios')
+function setColors(colors) {
+    deck.forEach(element => {
+        let color = Math.floor(Math.random() * colors.length)
+        element.innerHTML += '<span> ' + colors[color] + ' </span>';
+        colors.splice(color, 1)
     })
-    element.addEventListener('drop', () => {
+}
 
+function getColor(element) {
+    if (colorsShowed >= 1) {
+        restartColors()
+        colorsShowed = 0
+    }
+    let span = element.children[0]
+    element.style.background = span.innerText
+    colorsShowed++
+}
+
+function restartColors() {
+    deck.forEach(element => {
+        if (element.className !== 'matched')
+            element.style.background = 'black'
+    })
+}
+
+
+let i = 0
+deck.forEach(element => element.id = i++ + '')
+deck.forEach((element) => {
+
+    element.addEventListener('dragstart', ev => onDragElement = element.id)
+    element.addEventListener('dragover', ev => {
+        ev.preventDefault()
+    })
+    element.addEventListener('drop', (ev) => {
+        ev.preventDefault()
+
+        let targetElement = document.getElementById(onDragElement)
+        let targetColor = targetElement.children[0].innerText
+        let elementColor = element.children[0].innerText
+
+        if (onDragElement === element.id) return
+        if (targetColor === elementColor) {
+            console.log('Matched')
+            targetElement.style.background = targetColor
+            element.style.background = elementColor
+            targetElement.className  = 'matched'
+            element.className  = 'matched'
+            element.draggable = false
+            targetElement.draggable = false
+        }
     })
 })
 
-function startGame(deck) {
+setColors(colors)
 
-}
 
-function moveElement(element, ev) {
-    console.log('Hola')
-}
+
