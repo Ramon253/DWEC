0ba7e2dcc4fceef35e9376d551757b64f302ad79
Ramon253 <jgalazc253@g.educaand.es>
2024-02-09 01:11:06 +0100

    Practicas 7 terminadas

--- practicas/practicaTema7/ejercicio26/ejercicio26.js
+++ practicas/practicaTema7/ejercicio26/ejercicio26.js
@@ -1,38 +1,11 @@
-let deck = document.querySelectorAll('#deck > div')
-let colorsShowed = 0
-let onDragElement = ''
-let colors = [
-    '#FF5733',
-    '#33FF68',
-    '#3361FF',
-    '#B733FF',
-    '#FFFF33',
-    '#33FFFF',
-    '#FF33B7',
-    '#FF3333',
-    '#33FFAA',
-    '#B73333',
-    '#33B7FF',
-    '#FF33FF',
-    '#33FF33',
-    '#3333FF',
-    '#FFB733',
-    '#FF5733',
-    '#33FF68',
-    '#3361FF',
-    '#B733FF',
-    '#FFFF33',
-    '#33FFFF',
-    '#FF33B7',
-    '#FF3333',
-    '#33FFAA',
-    '#B73333',
-    '#33B7FF',
-    '#FF33FF',
-    '#33FF33',
-    '#3333FF',
-    '#FFB733'
-]
+let deck = document.querySelectorAll('#deck > div');
+let colorsShowed = 0;
+let matchedPairs = 0;
+let colors = ['#FF5733', '#33FF68', '#3361FF', '#B733FF', '#FFFF33', '#33FFFF', '#FF33B7', '#FF3333', '#33FFAA', '#B73333', '#33B7FF', '#FF33FF', '#33FF33', '#3333FF', '#FFB733', '#FF5733', '#33FF68', '#3361FF', '#B733FF', '#FFFF33', '#33FFFF', '#FF33B7', '#FF3333', '#33FFAA', '#B73333', '#33B7FF', '#FF33FF', '#33FF33', '#3333FF', '#FFB733'];
+let time = 0;
+let timer = setInterval(() => {
+    time++
+}, 1000)
 
 function setColors(colors) {
     deck.forEach(element => {
@@ -43,6 +16,7 @@ function setColors(colors) {
 }
 
 function getColor(element) {
+
     if (colorsShowed >= 1) {
         restartColors()
         colorsShowed = 0
@@ -61,29 +35,43 @@ function restartColors() {
 
 
 let i = 0
+let onDragElementId
 deck.forEach(element => element.id = i++ + '')
 deck.forEach((element) => {
-
-    element.addEventListener('dragstart', ev => onDragElement = element.id)
+    element.addEventListener('dragstart', ev => onDragElementId = element.id)
     element.addEventListener('dragover', ev => {
         ev.preventDefault()
     })
     element.addEventListener('drop', (ev) => {
         ev.preventDefault()
+        if (onDragElementId === element.id) return
 
-        let targetElement = document.getElementById(onDragElement)
+        let targetElement = document.getElementById(onDragElementId)
         let targetColor = targetElement.children[0].innerText
-        let elementColor = element.children[0].innerText
 
-        if (onDragElement === element.id) return
+        let elementColor = element.children[0].innerText
         if (targetColor === elementColor) {
             console.log('Matched')
+
             targetElement.style.background = targetColor
             element.style.background = elementColor
-            targetElement.className  = 'matched'
-            element.className  = 'matched'
+
+            targetElement.className = 'matched'
+            element.className = 'matched'
+
             element.draggable = false
             targetElement.draggable = false
+
+            matchedPairs++
+            if (matchedPairs === 15) {
+                clearInterval(timer)
+                let seconds = (time % 3600) % 60
+                let minutes = ((time - seconds) % 3600) / 60
+                let hours = (time - seconds - minutes * 60)/3600
+                document.getElementById('deck').innerHTML =
+                    `<h1>Has ganado</h1>` +
+                    `<h2>Tiempo tardado : ${hours}:${minutes}:${seconds}</h2>`
+            }
         }
     })
 })
